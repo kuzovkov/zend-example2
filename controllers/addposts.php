e8d455fdb382b95371626a8c7389c05e81eab40c
Your Name <you@example.com>
2016-11-13 00:30:33 +0300

    next

--- controllers/addposts.php
+++ controllers/addposts.php
@@ -9,10 +9,13 @@
     $n = (isset($_GET['n']))? intval($_GET['n']) : 0;
     $db = Zend_Registry::get('db');
     //var_dump($db);
-    $q = $db->select()->from('post', array('id', 'title', 'body', 'img', 'updated_at'))->limit(5, $n);
+    $q = $db->select()->from('post', array('id', 'title', 'body', 'img', 'updated_at'))->order(array('updated_at DESC', 'title'))->limit(5, $n);
     $posts = $db->fetchAll($q);
     for($i = 0; $i < count($posts); $i++){
-       $posts[$i]['tags'] = array('tag1', 'tag2');
+        $id = $posts[$i]['id'];
+        $q = $db->select()->from(array('t' => 'tag'),array('name'))->join(array('pt'=>'post_tags'), 't.id=pt.tag_id',array())->where('pt.post_id='.$id);
+        $tags = $db->fetchCol($q);
+        $posts[$i]['tags'] = $tags;
     }
 
     $smarty = Zend_Registry::get('smarty');
--- controllers/index.php
+++ controllers/index.php
@@ -2,10 +2,13 @@
 
     $db = Zend_Registry::get('db');
     //var_dump($db);
-    $q = $db->select()->from('post', array('id', 'title', 'body', 'img', 'updated_at'))->limit(5);
+    $q = $db->select()->from('post', array('id', 'title', 'body', 'img', 'updated_at'))->order(array('updated_at DESC', 'title'))->limit(5);
     $posts = $db->fetchAll($q);
     for($i = 0; $i < count($posts); $i++){
-       $posts[$i]['tags'] = array('tag1', 'tag2');
+        $id = $posts[$i]['id'];
+        $q = $db->select()->from(array('t' => 'tag'),array('name'))->join(array('pt'=>'post_tags'), 't.id=pt.tag_id',array())->where('pt.post_id='.$id);
+        $tags = $db->fetchCol($q);
+        $posts[$i]['tags'] = $tags;
     }
 
     $smarty = Zend_Registry::get('smarty');
